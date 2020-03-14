f54814cb79f75081e9b6f728d36ecd945c6994ff
ekzemplaro <linux@hi-ho.ne.jp>
2020-03-14 15:44:41 +0900

    Commit on Mar/14/2020

--- inspect/inspect.js
+++ inspect/inspect.js
@@ -1,10 +1,9 @@
 // ----------------------------------------------------------------------
 //	inspect.js
 //
-//					May/23/2017
+//					Mar/13/2020
 // ----------------------------------------------------------------------
 const file_json = "data.json"
-// const file_json = "https://github.com/kaz-ogiwara/covid19/tree/master/data/data.json"
 
 jQuery.getJSON(file_json,function (data)
 	{
@@ -70,23 +69,6 @@ function prog01 (graph_data)
 
 	const context = jQuery("#chart")
 	const chart = new Chart(context,config)
-}
-
-// ----------------------------------------------------------------------
-const barChartData = {
-	labels : ["2010","2011","2012","2013","2014","2015","2016"],
-	datasets : [
-		{
-		label: "AA",
-		lineTension: 0,
-		data : [31,25,45,5,20,19,33]
-		},
-		{
-		label: "BB",
-		lineTension: 0,
-		data : [25,10,54,77,32,9,78]
-		}
-	]
 }
 
 // ----------------------------------------------------------------------
