497228994fba316a4c9ee18365e4b51a6b9ca45c
ekzemplaro <linux@hi-ho.ne.jp>
2020-04-28 16:42:22 +0900

    Commit on Apr/28/2020

--- capacity_pref/capacity_pref.js
+++ capacity_pref/capacity_pref.js
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------
 //	capacity_pref.js
 //
-//					Apr/13/2020
+//					Apr/28/2020
 //
 // -----------------------------------------------------------------------
 jQuery (function ()
@@ -20,10 +20,12 @@ jQuery (function ()
 		str_out += "<th>Mar/7</th>"
 		str_out += "<th>Mar/24</th>"
 		str_out += "<th>Apr/06</th>"
+		str_out += "<th>Apr/15</th>"
 		str_out += "</tr>"
 		var sum_mar07 = 0
 		var sum_mar24 = 0
 		var sum_apr06 = 0
+		var sum_apr15 = 0
 
 		for (var key in data_aa)
 			{
@@ -34,11 +36,13 @@ jQuery (function ()
 			str_out += "<td>" + unit_aa.mar07 + "</td>"
 			str_out += "<td>" + unit_aa.mar24 + "</td>"
 			str_out += "<td>" + unit_aa.apr06 + "</td>"
+			str_out += "<td>" + unit_aa.apr15 + "</td>"
 			str_out += "</tr>"
 
 			sum_mar07 += parseInt(unit_aa.mar07)
 			sum_mar24 += parseInt(unit_aa.mar24)
 			sum_apr06 += parseInt(unit_aa.apr06)
+			sum_apr15 += parseInt(unit_aa.apr15)
 			}
 
 		str_out += "<tr>"
@@ -47,6 +51,7 @@ jQuery (function ()
 		str_out += "<td>" + sum_mar07 + "</td>"
 		str_out += "<td>" + sum_mar24 + "</td>"
 		str_out += "<td>" + sum_apr06 + "</td>"
+		str_out += "<td>" + sum_apr15 + "</td>"
 		str_out += "</tr>"
 		str_out += "</table>"
 
